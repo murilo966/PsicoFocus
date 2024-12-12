f83c79230a6f83bf01c782325b8954b0f6b5d24a
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-12-12 10:32:04 -0300

    Polindo e revisando as páginas

--- scripts.js
+++ scripts.js
@@ -1,12 +1,5 @@
 // scripts.js
 let currentIndex = 0;
-
-function showNextSlide() {
-    const slides = document.querySelector('.slides');
-    const totalSlides = slides.children.length;
-    currentIndex = (currentIndex + 1) % totalSlides;
-    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
+function showNextSlide() {const slides = document.querySelector('.slides'); const totalSlides = slides.children.length; currentIndex = (currentIndex + 1) % totalSlides; slides.style.transform = `translateX(-${currentIndex * 100}%)`;}
 setInterval(showNextSlide, 5000); // Muda de slide a cada 5 segundos
 
